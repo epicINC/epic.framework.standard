466870b770a7255216ec965869e1c46f459dd2af
JayU <github@slightboy.net>
2019-01-14 01:03:50 +0800

    temp

--- test/DevTest/Http/HttpTest.cs
+++ test/DevTest/Http/HttpTest.cs
@@ -7,12 +7,13 @@ namespace DevTest
 {
     class HttpTest
     {
+        public static readonly Uri Login = new Uri("/organizer.visitor.provider/login/index", UriKind.Relative);
         public static async void Test()
         {
-            var url = "http://emaster.eastfair.com/organizer.visitor.provider/login/index";
-
             var client = new System.Net.Http.HttpClient();
-            var result = await client.PostFormAsync(url, new { userName = "admin", password = "123456" });
+            client.BaseAddress = new Uri("http://emaster.eastfair.com");
+
+            var result = await client.PostFormAsync(Login, new { userName = "admin", password = "123456" });
 
 
         }
